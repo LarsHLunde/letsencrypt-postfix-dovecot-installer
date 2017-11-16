ab9a197ece4404ee89e6fb84ae8445ec355136b9
Lars Hudson Lunde <laffen47@yahoo.no>
2017-11-16 07:32:56 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # letsencrypt-postfix-dovecot-installer
-A Lua script that installs and configures both Postfix and dovecot with a Let's Encrypt Certbot certificate
+# WARNING: THIS SCRIPT DOES NOT WORK YET AT ALL
+Goal: To make a Lua script that installs and configures both Postfix and dovecot with a Let's Encrypt Certbot certificate
+
+## Requirements
+1. This requires that you have a registered domain, sub or otherwise, 
+this can be obtained for free from multiple sources including https://noip.com 
+A valid certificate can't be issued unless you have a domain.
+
---
+++ installer.lua
@@ -0,0 +1,40 @@
+-- Dovecot installer
+local commands = {}
+
+commands[#commands+1] = "openssl genrsa -des3 -out pyro.key"
+commands[#commands+1] = "chmod 600 pyro.key"
+commands[#commands+1] = "openssl req -new -key pyro.key -out pyro.csr"
+commands[#commands+1] = "openssl x509 -req -days 365 -in pyro.csr -signkey pyro.key -out pyro.crt"
+commands[#commands+1] = "openssl rsa -in pyro.key -out pyro.key.nopass"
+commands[#commands+1] = "mv pyro.key.nopass pyro.key"
+commands[#commands+1] = "openssl req -new -x509 -extensions v3_ca -keyout cakey.pem -out cacert.pem -days 365"
+commands[#commands+1] = "chmod 600 pyro.key"
+commands[#commands+1] = "chmod 600 cakey.pem"
+commands[#commands+1] = "mv pyro.key /etc/ssl/private/"
+commands[#commands+1] = "mv pyro.crt /etc/ssl/certs/"
+commands[#commands+1] = "mv cakey.pem /etc/ssl/private/"
+commands[#commands+1] = "mv cacert.pem /etc/ssl/certs/"
+commands[#commands+1] = "postconf -e \"myhostname = lars.ltd\""
+commands[#commands+1] = "postconf -e \"mydomain = lars.ltd\""
+commands[#commands+1] = "postconf -e \"myorigin = lars.ltd\""
+commands[#commands+1] = "postconf -e \"home_mailbox = Maildir/\""
+commands[#commands+1] = "postconf -e \"mailbox_command = \""
+commands[#commands+1] = "postconf -e \"smtpd_sasl_type = dovecot\""
+commands[#commands+1] = "postconf -e \"smptd_sasl_path = private/auth\""
+commands[#commands+1] = "postconf -e \"smptd_sasl_auth_enable = yes\""
+commands[#commands+1] = "postconf -e \"smtpd_tls_auth_only = no\""
+commands[#commands+1] = "postconf -e \"smtpd_use_tls = yes\""
+commands[#commands+1] = "postconf -e \"smtp_use_tls = yes\""
+commands[#commands+1] = "postconf -e \"smtp_tls_note_starttls_offer = yes\""
+commands[#commands+1] = "postconf -e \"smptd_tls_key_file = /etc/ssl/private/pyro.key\""
+commands[#commands+1] = "postconf -e \"smptd_tls_cert_file = /etc/ssl/certs/pyro.crt\""
+commands[#commands+1] = "postconf -e \"smptd_tls_CAfile = /etc/ssl/certs/cacert.pem\""
+commands[#commands+1] = "postconf -e \"smptd_tls_loglevel = 1\""
+commands[#commands+1] = "postconf -e \"smptd_tls_received_header = yes\""
+commands[#commands+1] = "postconf -e \"smptd_tls_session_cache_timeout = 3600s\""
+commands[#commands+1] = "service postfix restart"
+commands[#commands+1] = "apt-get install -y dovecot-common dovecot-imapd"
+
+for i = 1, #commands do
+	os.execute(commands[i])
+end
